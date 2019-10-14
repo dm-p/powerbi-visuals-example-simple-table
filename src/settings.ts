ca5b0a817a3f7f535f27fb6a3ad7e7eab0245751
dm-p <d.s.marsh@gmail.com>
2019-10-15 09:46:55 +1300

    Added settings for storing Tabulator configuration

--- src/settings.ts
+++ src/settings.ts
@@ -3,4 +3,11 @@
 import { dataViewObjectsParser } from "powerbi-visuals-utils-dataviewutils";
 import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
 
-export class VisualSettings extends DataViewObjectsParser {  }
+export class VisualSettings extends DataViewObjectsParser {
+    public tabulatorPersistentConfig: TabulatorPersistentConfig = new TabulatorPersistentConfig();
+}
+
+export class TabulatorPersistentConfig {
+    /** Array of widths for tabulator - this needs to be persisted/parsed as stirng to be compatible with the Power BI PrimitiveValue */
+        public columnWidths: string = '';
+}
