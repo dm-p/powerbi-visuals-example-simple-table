f6955fc1fcc3475420c5470e1c55dd7e8f888e74
dm-p <d.s.marsh@gmail.com>
2019-10-16 12:09:42 +1300

    Code to traverse data and render table using Tabulator rather than raw HTML

--- src/visual.ts
+++ src/visual.ts
@@ -24,16 +24,16 @@ export class Visual implements IVisual {
         /** Visual container */
             this.container = d3select.select(options.element)
                 .append('div')
-                    .append('table');
+                    .attr('id', 'example-table');
 
     }
 
     public update(options: VisualUpdateOptions) {
         this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-        
+
         /** Clear down existing plot */
             this.container.selectAll('*').remove();
-
+            
         /** Test 1: Data view has valid bare-minimum entries */
             let dataViews = options.dataViews;    
             console.log('Test 1: Valid data view...');
@@ -51,31 +51,59 @@ export class Visual implements IVisual {
         /** If we get this far, we can trust that we can work with the data! */
             let table = dataViews[0].table;
 
-        /** Add table heading row and columns */
-            let tHead = this.container
-                .append('tr');
-            table.columns.forEach(
-                (col) => {
-                    tHead
-                        .append('th')
-                            .text(col.displayName);
-                }
-            );
+        /** Set up table config */
+            let tableConfig: Tabulator.Options = {
 
-        /** Now add rows and columns for each row of data */
-            table.rows.forEach(
-                (row) => {
-                    let tRow = this.container
-                        .append('tr');
-                    row.forEach(
-                        (col) => {
-                            tRow
-                                .append('td')
-                                    .text(col.toString());
+                /** For columns, here is our strategy:
+                 *  - Iterate through table columns and assign the `displayName` to the `title`. This ensures that
+                 *      if we rename the field in the pane, then the title is correctly reflected. This is the
+                 *      same approach we took when building a raw HTML table.
+                 *  - Tabulator requires a `field` property. Because Power BI can conceivably allow us to add the
+                 *      same field with the same name multiple times, we will use the index assigned by Power BI 
+                 *      in the `dataView` as our column ID.
+                 */
+                    columns: table.columns.map((col) => ({
+                            title: col.displayName,
+                            field: col.index.toString()
+                        })
+                    ),
+                
+                /** For table data, our strategy is as follows:    
+                 *   -  Each row needs an ID - we will just make this row index + 1. We'll instantiate an object
+                 *        with this inside it, as we'll extend the object structure in the next part of our strategy.
+                 *   -  Because our columns are dynamic and tabulator uses JS object syntax, we will dynamically
+                 *        add keys/values for each column we come across to the object we created in the step above.
+                 *   -  Key names will match the index number we used in the `columns` object above. This allows
+                 *        Tabulator to recognise which column the value belongs to.
+                 *   -  We're currently just casting the value we find in each column for the row to a string and
+                 *        aren't considering the possibility of numbers of different formats at present.
+                 */
+                    data: table.rows.map((row, ri) => {
+
+                            /** Base object, with row ID */
+                                let fields = {
+                                    id: ri + 1
+                                };
+
+                            /** Add in key/value pairs for row data, ampping to column index */
+                                table.columns.map((col, ci) => {
+                                    fields[`${col.index}`] = row[ci].toString();
+                                });
+
+                            return fields;
                         }
-                    )
-                }
-            );
+                    ),
+
+                /** Anything else we want to do by default */
+                    height: '100%'
+
+            };
+
+        /** Add table to DOM and let Tabulator do its thing */
+            console.log('Rendering...');
+            console.log(Tabulator);
+            let grid = new Tabulator(this.container.node(), tableConfig);     
+
             console.log('Table rendered!');
         
     }
